4c3d37099525a6e12fef3ddb5b6ef46c570dfb6e
David Basil <info@basildavid.com>
2017-09-14 00:10:33 +0400

    add movie description and movie characters

--- routes/index.js
+++ routes/index.js
@@ -11,11 +11,23 @@ exports.home = function(req, res){
 // single movie
 exports.movie_single = function(req, res){
 	var episode_number = req.params.episode_number
-	res.send('This is the page for episode ' + episode_number)
+	var movies = moviesJSON.movies
+	if (episode_number >= 1 && episode_number <= 6){
+		var movie = movies[episode_number - 1]
+		var title = movie.title
+		var main_characters = movie.main_characters
+		res.render('movie_single', {
+			movies: movies,
+			movie: movie,
+			title: title,
+			main_characters: main_characters
+		})
+	} else {
+		res.send('This is not a page you\'re looking for.')
+	}
 }
 
 // 404 not found
 exports.notFound =  function(req, res){
 	res.send('404 Oops')
 }
-
